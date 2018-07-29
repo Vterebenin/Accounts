70facbe89e959094025c855d0fb441d4ffb3b157
Vterebenin <niceserver11@mail.ru>
2018-07-29 20:18:53 +0300

    add addons for more elegant code

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,8 @@ module Accounts
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # CUSTOM
+    config.react.addons = true
   end
 end
